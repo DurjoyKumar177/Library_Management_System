f140070f4fab28b62fe35c69956df0b16a7f1930
DurjoyKumar177 <durjoykumar177@gmail.com>
2024-11-21 19:44:39 +0600

    book_List, book_details template update

--- library_management/urls.py
+++ library_management/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from core.views import HomeView
+from books.views import BookListView
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
+    path('', BookListView.as_view(), name='home'),
     path('accounts/', include('accounts.urls')),
     path('books/', include('books.urls')),
     path('transactions/', include('transactions.urls')),
